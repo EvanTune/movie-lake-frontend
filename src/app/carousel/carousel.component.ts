3154428c52cb0ad1fa852556d115d58f3a6b5047
EvanTune <oyvlei@gmail.com>
2018-10-22 14:58:49 +0200

    Added arrows and dots to carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,23 +1,43 @@
-import {Component, ElementRef, HostListener, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { MOVIES } from '../mock-movies';
+import {Component, ElementRef, HostListener, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import {MovieService} from '../_services/movie.service';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  animations: [
+    trigger('arrowFade', [
+      state('show', style({
+        opacity: '1'
+      })),
+      state('hide', style({
+        opacity: '0'
+      })),
+      transition('show => hide', [
+        animate('0.2s ease-out')
+      ]),
+      transition('hide => show', [
+        animate('0.2s ease-in')
+      ])
+    ]),
+  ]
 })
 export class CarouselComponent implements OnInit {
 
   @ViewChild('carousel') carouselElement: ElementRef;
+  @ViewChild('dots') dotsElement: ElementRef;
 
-  movies = [{}];
-  currentPosition = 0;
-  windowWidth = 0;
+  movies: object[] = [{}];
+  currentPosition: number = 0;
+  windowWidth: number = 0;
+  showArrows: boolean;
+  canSlide: boolean = true;
 
   constructor(
     private movieService: MovieService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.setupTopMovies();
@@ -35,14 +55,25 @@ export class CarouselComponent implements OnInit {
   }
 
   next(e) {
-    this.currentPosition++;
-    this.carouselElement.nativeElement.scrollLeft += this.windowWidth;
-    console.log(this.windowWidth);
+    if (this.canSlide && this.currentPosition < this.movies.length - 1) {
+      this.currentPosition++;
+      this.carouselElement.nativeElement.scrollLeft += this.windowWidth;
+      this.canSlide = false;
+      setTimeout(() => {
+        this.canSlide = true;
+      }, 800);
+    }
   }
 
   prev(e) {
-    this.currentPosition--;
-    this.carouselElement.nativeElement.scrollLeft -= this.windowWidth;
+    if (this.canSlide && this.currentPosition > 0) {
+      this.currentPosition--;
+      this.carouselElement.nativeElement.scrollLeft -= this.windowWidth;
+      this.canSlide = false;
+      setTimeout(() => {
+        this.canSlide = true;
+      }, 800);
+    }
   }
 
   setWindowWidth() {
@@ -61,14 +92,30 @@ export class CarouselComponent implements OnInit {
 
       for (let i = 0; i < data['results'].length; i++) {
 
-        if (i > 7 && i < 18) {
+        if (i > 7 && i < 16) {
           this.movies.push(data['results'][i]);
           this.movies[count]['backdrop_path'] = 'https://image.tmdb.org/t/p/w1280' + this.movies[count]['backdrop_path'];
           count++;
         }
       }
-      console.log(this.movies);
+     this.setDotsLeft();
     });
+  }
+
+  setDotsLeft() {
+    const dotAmount = this.movies.length;
+    this.dotsElement.nativeElement.style.left = 'calc(50% - ' + dotAmount * 15 + 'px)';
+  }
+
+  setSlidePosition(index) {
+    if (this.canSlide) {
+      this.currentPosition = index;
+      this.carouselElement.nativeElement.scrollLeft = this.windowWidth * index;
+      this.canSlide = false;
+      setTimeout(() => {
+        this.canSlide = true;
+      }, 800);
+    }
   }
 
 }
